aeac3c0e0483248b4df40157438d2f47082fc43c
Rewan Khaled <83522163+RewanAbdulkariem@users.noreply.github.com>
2025-11-25 00:18:57 +0200

    Add Allure reports dependency

--- Automation/src/test/java/runners/TestRunner.java
+++ Automation/src/test/java/runners/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/features/epic3_product_details",
         glue = {"stepDefinitions", "hooks"},
-        plugin = {"pretty"}
+        plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
