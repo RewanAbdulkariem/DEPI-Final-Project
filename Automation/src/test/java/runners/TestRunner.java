a0b9c4202cc63b8919963f0793c37e6bc9e2f045
Rewan Khaled <83522163+RewanAbdulkariem@users.noreply.github.com>
2025-11-25 22:10:34 +0200

    add allure it was missed after merge

--- Automation/src/test/java/runners/TestRunner.java
+++ Automation/src/test/java/runners/TestRunner.java
@@ -4,10 +4,9 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/resources/features/epic1_registration_login/Register.feature",
-        glue = {"stepDefinitions", "hooks"}
-
+        features = "src/test/resources/features/epic7_account",
+        glue = {"stepDefinitions", "hooks"},
+        plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
 )
-public class TestRunner extends AbstractTestNGCucumberTests
-{
+public class TestRunner extends AbstractTestNGCucumberTests {
 }
