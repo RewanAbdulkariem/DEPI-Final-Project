9c4c2aa5c745408ed2a16095e8c331f4b2ec77dc
NohaElMandoh <nohamelmandoh@gmail.com>
2025-11-26 20:18:44 -0800

    admin featues

--- Automation/src/test/java/runners/TestRunner.java
+++ Automation/src/test/java/runners/TestRunner.java
@@ -4,8 +4,8 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/resources/features/epic7_account/AddressManagement.feature",
-        glue = {"stepDefinitions", "hooks"},
+        features = "src/test/resources/features/epic1_registration_login/Login.feature",
+        glue = {"stepDefinitions", "hooks"}  ,
         plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
 )
 public class TestRunner extends AbstractTestNGCucumberTests
