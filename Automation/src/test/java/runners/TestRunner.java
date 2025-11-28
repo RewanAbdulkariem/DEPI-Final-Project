42713e47c73e30d83ec37fbe0509fb5b2fddc26a
Rewan Khaled <83522163+RewanAbdulkariem@users.noreply.github.com>
2025-11-28 17:53:11 +0200

    update files

--- Automation/src/test/java/runners/TestRunner.java
+++ Automation/src/test/java/runners/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/resources/features/epic8_admin/AdminReports.feature",
+        features = "src/test/resources/features/epic8_admin",
         glue = {"stepDefinitions", "hooks"}  ,
         plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
         //tags = "not @admin"
