2674ab3768014550b99a60d0be888df92bb8b519
Rewan Khaled <83522163+RewanAbdulkariem@users.noreply.github.com>
2025-11-28 18:21:01 +0200

    add reports

--- Automation/src/test/java/runners/TestRunner.java
+++ Automation/src/test/java/runners/TestRunner.java
@@ -4,10 +4,10 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/resources/features/epic8_admin",
+        features = "src/test/resources/features",
         glue = {"stepDefinitions", "hooks"}  ,
         plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
-        //tags = "not @admin"
+        , tags = "not @admin"
 )
 public class TestRunner extends AbstractTestNGCucumberTests
 {
